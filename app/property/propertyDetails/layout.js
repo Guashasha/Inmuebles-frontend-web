5f02784ecaa46ab9e7634faf06263809510db435
Guashasha <davidcarrionr00@hotmail.com>
2026-01-06 23:00:49 -0600

    added dockerfile and fixed propertyDetails to not compile statically

--- app/property/propertyDetails/layout.js
+++ app/property/propertyDetails/layout.js
@@ -1,4 +1,5 @@
 import TopBar from "@components/topBar/TopBar.js";
+import { Suspense } from "react";
 
 export const metadata = {
   title: "Inmuebles a tu alcance",
@@ -9,7 +10,17 @@ export default function MainMenuLayout({ children }) {
   return (
     <div>
       <TopBar />
-      <main>{children}</main>
+      <main>
+        <Suspense
+          fallback={
+            <div style={{ padding: "50px", textAlign: "center" }}>
+              Cargando detalles...
+            </div>
+          }
+        >
+          {children}
+        </Suspense>
+      </main>
     </div>
   );
 }
